ce3a89dab15185cda6d9aad8d30cd4e03fc4dda1
Ryan Pablo <ryanprogramacaoedev@gmail.com>
2025-10-31 20:40:12 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("--- Tabuada ---");
+int numerodigitado;
+int n = 1;
+
+
+Console.Write("Digite o numero para fazer a tabuada dele: ");
+numerodigitado = Convert.ToInt32(Console.ReadLine());
+
+while (n < 10)
+{
+    Console.WriteLine($"{numerodigitado} X {n} = {numerodigitado * n}");
+    n = n + 1;
+}   
